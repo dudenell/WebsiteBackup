095beb964f446482801e86680f9f19cbdc78e21f
Dan <dan@danieljamescarr.com>
2017-11-29 13:39:29 -0500

    Removed old theme from code for better search

--- wp-content/themes/squarerootold/inc/plugins/fitwp-shortcodes/inc/tpl/advanced.php
+++
@@ -1,4 +0,0 @@
-<label class="button fitsc-advanced">
-	<?php _e( 'Advanced Settings', 'fitsc' ); ?>
-	<input ng-model="advanced" type="checkbox" class="hidden">
-</label>
--- wp-content/themes/squarerootold/sections/home_section.php
+++
@@ -1,9 +0,0 @@
-<div class="grid-container">
-<div class="grid-inner">
-<div class="overlay">
-<?php
-	the_content();
-?>
-</div>
-</div>
-</div>
